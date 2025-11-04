06287c5af3d18cccb31be4c3e18897819eb92e4a
hongru <z5547290@unsw.edu.au>
2025-11-04 22:12:34 +1100

    feat{ui}: add maintine

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider
+      defaultColorScheme="light"
+      theme={{ primaryColor: 'blue', defaultRadius: 'md' }}
+    >
+      <Notifications position="top-right" />
+
+      {}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
+  </StrictMode>
+);
