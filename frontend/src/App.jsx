a05a8a832fc4648c5a8493841b28f205dd00c35e
OWENhgvx <156950374+OWENhgvx@users.noreply.github.com>
2025-11-08 20:55:43 +1100

    feat: Add new routes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 // import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.jsx';
+// import Dashboard from './pages/Dashboard.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
+import CreateListing from './pages/CreatingListing.jsx';
+import ListingRequests from './pages/ListingRequests.jsx';
 
 
 function App() {
@@ -10,13 +12,19 @@ function App() {
   return (
     <Routes>
       {/* dashboard page */}
-      <Route path='/' element={<Dashboard />} />
+      {/* <Route path='/' element={<Dashboard />} /> */}
 
       {/* login page */}
       <Route path='/login' element={<Login />} />
 
       {/* register page */}
       <Route path='/register' element={<Register />} />
+
+      {/* create listing page */}
+      <Route path='/host/create-listing' element={<CreateListing />} />
+
+      {/* other routes can be added here */}
+      <Route path='host/listings/:listingId/requests' element={<ListingRequests />} />
 
     </Routes>
   )
