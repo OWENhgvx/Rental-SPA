997218b45bca57a486c24fc51ace65e97ef17ac4
hongru <z5547290@unsw.edu.au>
2025-11-08 15:42:05 +1100

    feat: Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/dates/styles.css'; 
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MantineProvider
-      defaultColorScheme="light"
-      theme={{ primaryColor: 'blue', defaultRadius: 'md' }}
-    >
+    <MantineProvider defaultColorScheme="light" theme={{ primaryColor: 'blue', defaultRadius: 'md' }}>
       <Notifications position="top-right" />
-
-      {}
       <BrowserRouter>
         <App />
       </BrowserRouter>
