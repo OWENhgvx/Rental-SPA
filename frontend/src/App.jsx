b97e81179cf57fe83696350040137c39222d4ca7
hongru <z5547290@unsw.edu.au>
2025-11-13 16:59:50 +1100

    feat: Update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 // import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import Dashboard from './pages/Dashboard.jsx';
+import { Routes, Route} from 'react-router-dom';
 import Layout from './Layout';
+import Dashboard from './pages/Dashboard.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import CreateListing from './pages/CreatingListing.jsx';
 import ListingRequests from './pages/ListingRequests.jsx';
 import HostListings from './pages/HostListings.jsx';
+import AvailabilityPage from './pages/AvailabilityPage.jsx';  
+import ListingBook from './pages/ListingBook.jsx';
 
-
-function App() {
+export default function App() {
   
   return (
     <Routes>
+
       <Route element={<Layout />}>
+
         {/* dashboard page */}
-        {/* <Route path='/' element={<Dashboard />} /> */}
-
-
-        {/* register page */}
-        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Dashboard />} />
 
         {/* create listing page */}
         <Route path='/host/create-listing' element={<CreateListing />} />
@@ -27,22 +26,28 @@ function App() {
         {/* edit listing page */}
         <Route path="/host/listings/edit/:id" element={<CreateListing />} />
 
-        {/* creat listing request page */}
-        <Route path='/host/listings/:listingId/requests' element={<ListingRequests />} />
-        
+        {/* other routes can be added here */}
+        <Route path='host/listings/:listingId/requests' element={<ListingRequests />} />
+
+        {/* listing page  */}
+        <Route path="/listings/:listingId" element={<ListingBook />} />
+
         {/* host listings page */}
         <Route path='/host/listings' element={<HostListings />} />
-      </Route>
 
+        {/* availability page */}
+        <Route path='/host/listings/:id/availability' element={<AvailabilityPage />} />
+
+      </Route>
+      
       {/* login page */}
       <Route path='/login' element={<Login />} />
 
       {/* register page */}
       <Route path='/register' element={<Register />} />
 
-
     </Routes>
   )
 }
 
-export default App;
+
