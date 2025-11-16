a5108383d49b51b38c7991fb9b65a81b13cabbdf
hongru <z5547290@unsw.edu.au>
2025-11-16 17:26:04 +1100

    feat: Add booking function

--- frontend/src/api/BookingApi.js
+++ frontend/src/api/BookingApi.js
@@ -88,6 +88,89 @@ export async function GetListingBookingDetail(token,useremail,listingid){
 
 }
 
+// get current user booking detail
+export async function GetUserBookingDetail(token,useremail){
+
+  const res = await fetch(`${NET_ADDRESS}/bookings`,{
+    method: 'GET',
+    headers: {
+      'Content-type':'application/json',
+      'Authorization':token
+    }
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+
+  // user booked in this listing
+  const bookList=[];
+  data.bookings.forEach(element => {
+
+    if(element.owner===useremail){
+      bookList.push(element)
+    }
+  });
+
+  return bookList;
+
+}
+
+//get all listings of this host
+export async function GetAllHostListing(hostemail){
+
+  const res = await fetch(`${NET_ADDRESS}/listings`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+
+  const listings=[];
+
+  data.listings.forEach((listing)=>{
+    if(listing.owner===hostemail) listings.push(listing.id);
+  })
+
+  return listings ?? [];
+
+}
+
+// the host can get their own listing's all booking,
+export async function GetHostBookingDetail(token,hostemail){
+
+  const res = await fetch(`${NET_ADDRESS}/bookings`,{
+    method: 'GET',
+    headers: {
+      'Content-type':'application/json',
+      'Authorization':token
+    }
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+
+  const hostListings= await GetAllHostListing(hostemail);
+
+  const bookList=[];
+  data.bookings.forEach(element => {
+
+    if(hostListings.includes(element.id)){
+      bookList.push(element)
+    }
+  });
+
+  return bookList;
+
+}
+
+
 // get current success booking detail,user email is user's email,not the house owner
 export async function GetSuccessListingBookingDetail(token,useremail,listingid){
 
