32d6c135fb2700d0ef7f1ce5135cb887a47bf4e2
OWENhgvx <156950374+OWENhgvx@users.noreply.github.com>
2025-12-24 23:22:41 +0800

    fix:deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,10 +9,6 @@ import '@mantine/notifications/styles.css';
 import '@mantine/dates/styles.css'; 
 import App from './App.jsx';
 
-// 把环境变量挂载到全局 window 对象上，方便我们调试
-window.VITE_ENV_DEBUG = import.meta.env;
-console.log('Build Environment:', import.meta.env);
-
 // Render the main application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
